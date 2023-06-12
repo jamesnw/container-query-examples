08335fcadf9a215100d1597389cca9c70c63e271
James Stuckey Weber <jamesnw@gmail.com>
2023-06-12 16:10:02 -0400

    Add aspect ratio and cleanup

--- script.js
+++ script.js
@@ -26,8 +26,24 @@ exampleInfo.forEach(example => {
     anchor.dataset.name = example.id;
     const li = document.createElement('li');
     li.append(anchor)
-
     toc.append(li)
+});
+
+const radio = document.getElementById('radio');
+exampleInfo.forEach((example, index) => {
+    const input = document.createElement('input');
+    input.type = 'radio';
+    input.name = 'query';
+    input.value = example.id;
+    input.id = 'radio' + example.id;
+    if (index === 0) input.checked = 'checked';
+    const label = document.createElement('label');
+    label.setAttribute('for', 'radio' + example.id);
+    label.innerText = example.name;
+
+    radio.append(input);
+    radio.append(label);
+    radio.append(document.createElement('br'));
 });
 
 const styleElement = document.createElement('style');
@@ -49,6 +65,14 @@ exampleInfo.forEach(({ name, id, type }) => {
     a[data-name='${id}'] {
         color: var(--target-color);
         background: var(--accent-vibrant);
+    }
+}
+@container radio (${name}){
+    #radio-wrapper:has([value='${id}']:checked) #yes{
+        display: block;
+    }
+    #radio-wrapper:has([value='${id}']:checked) #no{
+        display: none;
     }
 }
     `
